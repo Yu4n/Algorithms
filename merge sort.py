2ff574a05358c9a8740a97cbf846a9b5862835ee
Yu4n <swa1n.suen@gmail.com>
2020-06-16 16:31:47 +0800

    Init
    Init

---
+++ merge sort.py
@@ -0,0 +1,37 @@
+import math
+
+
+def merge_sort(a, p, r):
+    if p < r:
+        q = math.floor((p + r) / 2)
+        merge_sort(a, p, q)
+        merge_sort(a, q + 1, r)
+        merge(a, p, q, r)
+
+
+def merge(a, p, q, r):
+    n1 = q - p + 1  # Length of subarray a[p, q]
+    n2 = r - q  # Length of subarray a[q + 1, r]
+    left = []
+    right = []
+    for i in range(n1):
+        left.append(a[p + i])  # copy the subarray A[p, q] into L[1, n1]
+    for j in range(n2):
+        right.append(a[q + j + 1])  # copy the subarray A[q + 1, r] into R[1, n1]
+    left.append(math.inf)
+    right.append(math.inf)
+    i, j = 0, 0
+    for k in range(p, r + 1):
+        if left[i] <= right[j]:
+            a[k] = left[i]
+            i = i + 1
+        else:
+            a[k] = right[j]
+            j = j + 1
+
+
+ls = [5, 4, 3, 2, 1, 0, -1, -2, -3, -4]
+# merge(ls, 1, 1, 2)
+print(ls)
+merge_sort(ls, 0, 9)
+print(ls)
---
+++ selection sort.py
@@ -0,0 +1,15 @@
+def selection_sort(ls):
+    for j in range(len(ls)-1):
+        min = j
+        for i in range(j + 1, len(ls)):
+            if ls[i] < ls[min]:
+                min = i
+        temp = ls[j]
+        ls[j] = ls[min]
+        ls[min] = temp
+
+
+A = [5, 4, 3, 2, 1]
+selection_sort(A)
+for a in A:
+    print(a)
