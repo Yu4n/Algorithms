79349c3ff4f47c16f0693b38527846fe487c5230
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-06-19 09:28:22 +0800

    update

--- Insertion sort.py
+++ Insertion sort.py
@@ -1,6 +1,3 @@
-# Python program for implementation of Insertion Sort
-
-# Function to do insertion sort
 def insertionSort(arr):
     # Traverse through 1 to len(arr)
     for i in range(1, len(arr)):
@@ -19,10 +16,31 @@ def insertionSort(arr):
     # Driver code to test above
 
 
+def insertionSortRecursive(arr, n):
+    # base case
+    if n <= 1:
+        return
+
+    # Sort first n-1 elements
+    insertionSortRecursive(arr, n - 1)
+    '''Insert last element at its correct position 
+        in sorted array.'''
+    key = arr[n - 1]
+    j = n - 2
+
+    # Move elements of arr[0..i-1], that are
+    # greater than key, to one position ahead
+    # of their current position
+    while (j >= 0 and arr[j] > key):
+        arr[j + 1] = arr[j]
+        j = j - 1
+
+    arr[j + 1] = key
+
+
 arr = [12, 11, 13, 5, 6]
 insertionSort(arr)
-print("Sorted array is:")
-for j in range(len(arr)):
-    print("%d" % arr[j])
-
-    # This code is contributed by Mohit Kumra
+print(arr)
+arr2 = [6, 5, 4, 3, 2, 1, 0, -1]
+insertionSortRecursive(arr2, len(arr2))
+print(arr2)
