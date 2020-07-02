29373214895f026e99e1fa9104a8c5b59672a7cc
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-07-02 13:02:52 +0800

    update

--- maxSubArray.py
+++ maxSubArray.py
@@ -53,12 +53,13 @@ def maxSubArray(nums):
 
 def maxSubArray2(nums):
     """Smallest memory needed."""
-    localSum = -math.inf
-    globalSum = -math.inf
-    for n in nums:
-        localSum = max(localSum + n, n)
-        globalSum = max(globalSum, localSum)
-    return globalSum
+    max_so_far = nums[0]
+    max_ending_here = nums[0]
+
+    for i in range(1, len(nums)):
+        max_ending_here = max(max_ending_here + nums[i], nums[i])
+        max_so_far = max(max_so_far, max_ending_here)
+    return max_so_far
 
 
 ls = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
