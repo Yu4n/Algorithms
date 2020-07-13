a5fdb68b44837484874b41515ca32116084f4a5d
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-07-14 00:29:44 +0800

    My own code of heapsort and one flaw found.

--- heapsort.py
+++ heapsort.py
@@ -41,7 +41,36 @@ def heapSort(arr):
         heapify(arr, i, 0)
 
 
-arr = [12, 11, 13, 5, 6, 20, 20, 50, 7, 4, 3, 2, 1, -1]
-heapSort(arr)
-n = len(arr)
-print(arr)
+def max_heapify(a, n, i):
+    heapSize = n
+    left = 2 * i + 1
+    right = 2 * i + 2
+    if left < heapSize and a[i] < a[left]:
+        largest = left
+    else:
+        largest = i
+    # if a[largest] < a[right] and right < heapSize:
+    # Be careful of the conditional statement above which causes list out of range!
+    if right < heapSize and a[largest] < a[right]:
+        largest = right
+    if largest != i:
+        a[i], a[largest] = a[largest], a[i]
+        max_heapify(a, n, largest)
+
+
+def build_max_heap(a):
+    for i in range(len(a) // 2 - 1, -1, -1):
+        max_heapify(a, len(a) - 1, i)
+
+
+def heapsort(a):
+    build_max_heap(a)
+    for i in range(len(a) - 1, 0, -1):
+        a[0], a[i] = a[i], a[0]
+        max_heapify(a, i, 0)
+
+
+if __name__ == '__main__':
+    arr = [12, 11, 13, 20, 20, 50, 7, 4, 3, 2, 1, -1]
+    heapsort(arr)
+    print(arr)
