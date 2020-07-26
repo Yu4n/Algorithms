5933c673bcf7e9aef0c32146a70ba39acd28385b
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-07-26 21:00:25 +0800

    Some operations on linked list.

--- linked_list.py
+++ linked_list.py
@@ -1,6 +1,6 @@
 class Node:
     def __init__(self, data):
-        self.data = data
+        self.val = data
         self.next = None
 
 
@@ -9,11 +9,63 @@ class LinkedList:
         self.head = None
 
 
+def list_search(ls, key):
+    x = ls.head
+    while x is not None and key != x.val:
+        x = x.next
+    try:
+        if x.val == key:
+            print(f"{key} found!")
+    except AttributeError:
+        print("Not found!")
+
+
+def list_insert(ls, pos, key):  # Index starts at 1.
+    new = Node(key)
+    x = ls.head
+    for i in range(pos - 1):
+        x = x.next
+    new.next = x.next
+    x.next = new
+
+
+def list_delete(ls, pos):
+    x = ls.head
+    for i in range(pos - 1):
+        x = x.next
+    temp = x
+    temp.next = x.next.next
+
+
+def list_edit(ls, pos, key):
+    x = ls.head
+    for i in range(pos - 1):
+        x = x.next
+    x.next.val = key
+
+
 if __name__ == '__main__':
     llist = LinkedList()
-    llist.head = Node(1)
+    llist.head = Node(None)
+    first = Node(1)
     second = Node(2)
     third = Node(3)
-    llist.head.next = second
+    llist.head.next = first
+    first.next = second
     second.next = third
-    print(second.next.data)
+    list_insert(llist, 4, 100)
+    n = llist.head
+    while n is not None:
+        print(n.val)
+        n = n.next
+    list_delete(llist, 1)
+    n = llist.head
+    while n is not None:
+        print(n.val)
+        n = n.next
+    list_search(llist, 100)
+    list_edit(llist, 3, 200)
+    n = llist.head
+    while n is not None:
+        print(n.val)
+        n = n.next
