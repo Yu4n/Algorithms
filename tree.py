bc97a24f211dfa78244eb29a54681310dce0bfb8
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-08-16 16:53:49 +0800

    Inorder traversal

--- tree.py
+++ tree.py
@@ -27,6 +27,28 @@ def print_binary_tree_non_recursive(tree):
             ls.append(x.right)
 
 
+def inorder_tree_walk(root):
+    if root:
+        inorder_tree_walk(root.left)
+        print(root.val)
+        inorder_tree_walk(root.right)
+
+
+def inorder_non_recursive(root):
+    stack = []
+    curr = root
+    while True:
+        if curr:
+            stack.append(curr)
+            curr = curr.left
+        elif stack:
+            curr = stack.pop()
+            print(curr.val, end=' ')
+            curr = curr.right
+        else:
+            break
+
+
 if __name__ == '__main__':
     Root = TreeNode(1)
     Root.left = TreeNode(2)
@@ -35,4 +57,5 @@ if __name__ == '__main__':
     Root.left.right = TreeNode(5)
     Root.right.left = TreeNode(6)
     Root.right.right = TreeNode(7)
-    print_binary_tree_non_recursive(Root)
+    # print_binary_tree_non_recursive(Root)
+    inorder_non_recursive(Root)
