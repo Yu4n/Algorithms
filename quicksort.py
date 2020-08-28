1953171e7309f177ccac700b4ca0c8230fb64331
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-08-28 09:08:10 +0800

    Tweak partition for better understanding.

--- quicksort.py
+++ quicksort.py
@@ -10,17 +10,17 @@ def quicksort(a, p, r):
 
 def partition(a, p, r):
     pivot = a[r]
-    i = p - 1
+    i = p
     # Loop invariant:
     #   1. If p <= k <= i, then a[k] <= pivot.
     #   2. If i + 1 <= k <= j - 1, then a[k] > pivot.
     #   3. If k = r, then a[k] = pivot.
-    for j in range(p, r):  # j is from p to r - 1(inclusive).
+    for j in range(p, r):  # j is from p to r - 1 (inclusive).
         if a[j] <= pivot:
-            i += 1
             a[i], a[j] = a[j], a[i]
-    a[i + 1], a[r] = a[r], a[i + 1]
-    return i + 1
+            i += 1
+    a[i], a[r] = a[r], a[i]
+    return i
 
 
 def randomized_partition(a, p, r):
@@ -62,6 +62,6 @@ def hoare_partition(a, p, r):
             return j
 
 
-A = [2, 8, 7, 1, 3, 5, 6, 4]
+A = [2, 8, 7, 1, 3, 5, -2, 6, 4]
 hoare_quicksort(A, 0, len(A) - 1)
 print(A)
