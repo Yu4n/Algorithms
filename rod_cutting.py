3a16acdbc4acf704a24ff887320860ae1e89d4a0
Yu4n <swa1@gmail.com>
2020-10-19 16:10:46 +0800

    Add notes about DP (CLRS p.365)

--- rod_cutting.py
+++ rod_cutting.py
@@ -1,3 +1,38 @@
+""" The dynamic-programming method works as follows. Having observed that a
+    naive recursive solution is inefficient because it solves the same subproblems re-
+    peatedly, we arrange for each subproblem to be solved only once, saving its solu-
+    tion. If we need to refer to this subproblem’s solution again later, we can just look it
+    up, rather than recompute it. Dynamic programming thus uses additional memory
+    to save computation time; it serves an example of a time-memory trade-off. The
+    savings may be dramatic: an exponential-time solution may be transformed into a
+    polynomial-time solution. A dynamic-programming approach runs in polynomial
+    time when the number of distinct subproblems involved is polynomial in the input
+    size and we can solve each such subproblem in polynomial time.
+
+    There are usually two equivalent ways to implement a dynamic-programming
+    approach. We shall illustrate both of them with our rod-cutting example.
+
+    The first approach is top-down with memoization.2 In this approach, we write
+    the procedure recursively in a natural manner, but modified to save the result of
+    each subproblem (usually in an array or hash table). The procedure now first checks
+    to see whether it has previously solved this subproblem. If so, it returns the saved
+    value, saving further computation at this level; if not, the procedure computes the
+    value in the usual manner. We say that the recursive procedure has been memoized;
+    it “remembers” what results it has computed previously.
+
+    The second approach is the bottom-up method. This approach typically depends
+    on some natural notion of the “size” of a subproblem, such that solving any par-
+    ticular subproblem depends only on solving “smaller” subproblems. We sort the
+    subproblems by size and solve them in size order, smallest first. When solving a
+    particular subproblem, we have already solved all of the smaller subproblems its
+    solution depends upon, and we have saved their solutions. We solve each sub-
+    problem only once, and when we first see it, we have already solved all of its
+    prerequisite subproblems.
+
+    These two approaches yield algorithms with the same asymptotic running time,
+    except in unusual circumstances where the top-down approach does not actually
+    recurse to examine all possible subproblems. The bottom-up approach often has
+    much better constant factors, since it has less overhead for procedure calls."""
 import math
 
 
