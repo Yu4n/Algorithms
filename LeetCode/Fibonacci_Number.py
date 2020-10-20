f52b67e5a5eee847f2175c6c770cae0f6fd66b47
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-10-20 13:10:10 +0800

    Fibonacci_Number.py

--- LeetCode/Fibonacci_Number.py
+++ LeetCode/Fibonacci_Number.py
@@ -7,3 +7,13 @@ class Solution:
             r = f[i - 2] + f[i - 1]
             f.append(r)
         return f[-1]
+
+    def fib1(self, n: int) -> int:
+        a,b = 0,1
+        if n==0 or n==1:
+            return n
+        for i in range(n):
+            total = a + b
+            b=a
+            a= total
+        return total
