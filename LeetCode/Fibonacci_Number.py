8e1dfee2169ab22af499d5060575f63a9dd50ce7
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-10-20 20:42:15 +0800

    Reformat a solution from leetcode

--- LeetCode/Fibonacci_Number.py
+++ LeetCode/Fibonacci_Number.py
@@ -2,6 +2,7 @@ class Solution:
     """ The Fibonacci numbers are defined by recurrence 3.22. Give an O(n)
         time dynamic-programming algorithm to compute the nth Fibonacci number.
         Draw the subproblem graph. How many vertices and edges are in the graph? (CLRS p.370)"""
+
     def fib(self, N: int) -> int:
         f = [0, 1]
         if N <= 1:
@@ -12,11 +13,11 @@ class Solution:
         return f[-1]
 
     def fib1(self, n: int) -> int:
-        a,b = 0,1
-        if n==0 or n==1:
+        a, b = 0, 1
+        if n == 0 or n == 1:
             return n
         for i in range(n):
             total = a + b
-            b=a
-            a= total
+            b = a
+            a = total
         return total
