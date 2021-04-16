287138f5870291db4e070a07308f568e74e6462e
Yu4n <28648731+yu4n@users.noreply.github.com>
2021-04-16 21:47:11 +0800

    rotate.py

---
+++ LeetCode_in_py/rotate.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rotate(self, matrix):
+        matrix.reverse()
+        for i in range(len(matrix) - 1):
+            for j in range(i + 1, len(matrix)):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    def rotate_(self, matrix):
+        n = len(matrix[0])
+        for i in range(n // 2 + n % 2):
+            for j in range(n // 2):
+                tmp = matrix[n - 1 - j][i]
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1]
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i]
+                matrix[j][n - 1 - i] = matrix[i][j]
+                matrix[i][j] = tmp
