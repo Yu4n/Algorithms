0df27065c547c59e118aed1dc841c3f7ade32a5e
Yu4n <28648731+yu4n@users.noreply.github.com>
2021-04-19 22:26:32 +0800

    singleNumberII.py Bitwise

--- LeetCode_in_py/singleNumberII.py
+++ LeetCode_in_py/singleNumberII.py
@@ -7,3 +7,16 @@ class Solution:
         for i in c:
             if c[i] == 1:
                 return i
+
+    def singleNumberBit(self, nums):
+        res = 0
+        for i in range(32):
+            count = 0
+            for num in nums:
+                count += (num >> i) & 1
+            rem = count % 3
+            if i == 31 and rem:
+                res -= 1 << 31
+            else:
+                res |= rem * (1 << i)
+        return res
