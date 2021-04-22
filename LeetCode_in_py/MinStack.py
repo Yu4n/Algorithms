8449aa2ef0400f4afcf42e73ac34a0d494f6d5c6
Yu4n <28648731+yu4n@users.noreply.github.com>
2021-04-22 22:03:50 +0800

    MinStack.py O(1) space complexity

--- LeetCode_in_py/MinStack.py
+++ LeetCode_in_py/MinStack.py
@@ -1,18 +1,29 @@
 class MinStack:
     def __init__(self):
         self.stack = []
-        self.min_stack = [math.inf]
+        self.min_value = -1
 
     def push(self, x: int) -> None:
-        self.stack.append(x)
-        self.min_stack.append(min(x, self.min_stack[-1]))
+        if not self.stack:
+            self.stack.append(0)
+            self.min_value = x
+        else:
+            diff = x-self.min_value
+            self.stack.append(diff)
+            self.min_value = self.min_value if diff > 0 else x
 
     def pop(self) -> None:
-        self.stack.pop()
-        self.min_stack.pop()
+        if self.stack:
+            diff = self.stack.pop()
+            if diff < 0:
+                top = self.min_value
+                self.min_value = top - diff
+            else:
+                top = self.min_value + diff
+            return top
 
     def top(self) -> int:
-        return self.stack[-1]
+        return self.min_value if self.stack[-1] < 0 else self.stack[-1] + self.min_value
 
     def getMin(self) -> int:
-        return self.min_stack[-1]
+        return self.min_value if self.stack else -1
