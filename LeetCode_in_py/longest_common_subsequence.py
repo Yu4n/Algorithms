819b10f31ecc40afede89b8d4fa7e996d254fe7e
Yu4n <28648731+yu4n@users.noreply.github.com>
2021-05-15 10:27:59 +0800

    longest_common_subsequence.py

--- LeetCode_in_py/longest_common_subsequence.py
+++ LeetCode_in_py/longest_common_subsequence.py
@@ -1,19 +1,10 @@
 class Solution:
-    def longestCommonSubsequence(self, str1, str2) -> int:
-        m, n = len(str1), len(str2)
-        # 构建 DP table 和 base case
-        dp = [[0] * (n + 1) for _ in range(m + 1)]
-        # 进行状态转移
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
-                if str1[i - 1] == str2[j - 1]:
-                    # 找到一个 lcs 中的字符
-                    dp[i][j] = 1 + dp[i-1][j-1]
-                else:
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        n = len(text1)
+        m = len(text2)
+        dp = [[0] * (m + 1) for _ in range(n + 1)]
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                # wrong! max(dp[i - 1][j], dp[i][j - 1]) + 1 if text1[i - 1] == text2[j - 1]
+                dp[i][j] = dp[i - 1][j - 1] + 1 if text1[i - 1] == text2[j - 1] else max(dp[i - 1][j], dp[i][j - 1])
         return dp[-1][-1]
-
-
-x = Solution()
-print(x.longestCommonSubsequence("abcde", "ace"))
