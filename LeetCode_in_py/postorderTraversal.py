f6795502d268bc5e129e8c7ea84cae78ce77496a
Yu4n <28648731+yu4n@users.noreply.github.com>
2021-05-15 15:10:17 +0800

    postorderTraversal.py

---
+++ LeetCode_in_py/postorderTraversal.py
@@ -0,0 +1,15 @@
+class Solution:
+    def postorderTraversal(self, root):
+        traversal, stack = [], [(root, False)]
+        while stack:
+            node, visited = stack.pop()
+            if node:
+                if visited:
+                    # add to result if visited
+                    traversal.append(node.val)
+                else:
+                    # post-order
+                    stack.append((node, True))
+                    stack.append((node.right, False))
+                    stack.append((node.left, False))
+        return traversal
