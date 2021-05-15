55899eb5072bf0795ec6147eba9372e7fd2ab719
Yu4n <28648731+yu4n@users.noreply.github.com>
2021-05-15 09:22:51 +0800

    longestPalindromeSubstring.py my code

--- LeetCode_in_py/longestPalindromeSubstring.py
+++ LeetCode_in_py/longestPalindromeSubstring.py
@@ -1,22 +1,26 @@
 # https://leetcode-cn.com/problems/longest-palindromic-substring/
 class Solution:
-    def expandAroundCenter(self, s, left, right):
-        while left >= 0 and right < len(s) and s[left] == s[right]:
+    def centerExpand(self, s, left, right):
+        while 0 <= left <= right < len(s) and s[left] == s[right]:
             left -= 1
             right += 1
         return left + 1, right - 1
 
     def longestPalindrome(self, s: str) -> str:
-        start, end = 0, 0
+        start = end = 0
         for i in range(len(s)):
-            left1, right1 = self.expandAroundCenter(s, i, i)
-            left2, right2 = self.expandAroundCenter(s, i, i + 1)
+            left1, right1 = self.centerExpand(s, i, i)
+            print(left1, right1)
+            left2, right2 = self.centerExpand(s, i, i + 1)
+            print(left2, right2)
             if right1 - left1 > end - start:
                 start, end = left1, right1
             if right2 - left2 > end - start:
                 start, end = left2, right2
-        return s[start: end + 1]
+            print(start, end)
+            print()
+        return s[start:end + 1]
 
 
 x = Solution()
-print(x.longestPalindrome("babad"))
+print(x.longestPalindrome("bb"))
