59663bf8d1eac804c2561799820f8da5e989e5a6
Yu4n <28648731+yu4n@users.noreply.github.com>
2021-05-22 21:05:44 +0800

    zigzag.py refactored

--- LeetCode_in_py/zigzag.py
+++ LeetCode_in_py/zigzag.py
@@ -2,20 +2,11 @@ class Solution:
     def convert(self, s: str, numRows: int) -> str:
         if numRows == 1:
             return s
-        lsOfString = [""] * numRows
+        strings = [""] * numRows
         i = 0
         down = False
         for c in s:
-            lsOfString[i] += c
-            if down:
-                i -= 1
-                if i == 0:
-                    down = False
-            else:
-                i += 1
-                if i == numRows - 1:
-                    down = True
-        res = ""
-        for ss in lsOfString:
-            res += ss
-        return res
+            strings[i] += c
+            i += -1 if down else 1
+            down = not down if i == 0 or i == numRows - 1 else down
+        return "".join(strings)
