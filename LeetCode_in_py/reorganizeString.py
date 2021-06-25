8b86ae6c9abdc1029615b07a126cf5c713afe2ce
Yu4n <28648731+Yu4n@users.noreply.github.com>
2021-06-25 21:12:03 +0800

    reorganizeString.py

--- LeetCode_in_py/reorganizeString.py
+++ LeetCode_in_py/reorganizeString.py
@@ -3,6 +3,22 @@ from collections import Counter
 
 class Solution:
     def reorganizeString(self, s: str) -> str:
+        n = len(s)
+        freq = Counter(s)
+        freq = sorted(freq.items(), key=lambda k: -k[1])
+        if freq[0][1] >= n / 2 + 1:
+            return ""
+        res = [""] * n
+        i = 0
+        for x, fx in freq:
+            for _ in range(fx):
+                if i >= n:
+                    i = 1
+                res[i] = x
+                i += 2
+        return "".join(res)
+
+    def reorganizeString1(self, s: str) -> str:
         length = len(s)
         if length < 2:
             return s
@@ -28,3 +44,7 @@ class Solution:
                 count -= 1
                 even += 2
         return "".join(res)
+
+
+sln = Solution()
+print(sln.reorganizeString("abbbaa"))
