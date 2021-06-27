33f25070ee539ff87acc268cc8aaf2600a07fedc
Yu4n <28648731+Yu4n@users.noreply.github.com>
2021-06-27 21:58:58 +0800

    permute.py quicker backtrack

--- LeetCode_in_py/permute.py
+++ LeetCode_in_py/permute.py
@@ -1,4 +1,18 @@
 class Solution:
+    def permute(self, nums):
+        res = []
+        self.dfs(nums, [], res)
+        return res
+
+    def dfs(self, nums, path, res):
+        if not nums:
+            res.append(path)
+            return
+        for i in range(len(nums)):
+            self.dfs(nums[:i] + nums[i + 1:], path + [nums[i]], res)
+
+
+class Sln:
     def __init__(self):
         self.res = []
 
@@ -8,13 +22,13 @@ class Solution:
         return self.res
 
     def backtrack(self, nums, track):
-        if len(nums) == len(track):
+        if len(nums) == len(track):  # if the condition is satisfied
             self.res.append(track[:])  # deep copy track and append
             print(self.res)
             return
         for i in range(len(nums)):
             if nums[i] in track:
                 continue
-            track.append(nums[i])
+            track.append(nums[i])  # decide
             self.backtrack(nums, track)
-            track.pop()
+            track.pop()  # undo decision
