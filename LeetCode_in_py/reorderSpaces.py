651adc2f4b3ce29998f15c12294852c3b011a88c
Yu4n <28648731+Yu4n@users.noreply.github.com>
2021-10-17 20:06:56 +0800

    reorderSpaces.py shortened

--- LeetCode_in_py/reorderSpaces.py
+++ LeetCode_in_py/reorderSpaces.py
@@ -4,18 +4,8 @@ class Solution:
         words = text.split()
         if len(words) == 1:
             return words[0] + ' ' * space_count
-        print(space_count)
-        average_spaces = (space_count // (len(words) - 1)) * ' '
-        tail_space = space_count % (len(words) - 1)
-        print(tail_space)
-        res = average_spaces.join(words)
-
-        # for word in words:
-        #     res += word + ' ' * average_spaces
-        # res.strip()
-        # print(res)
-        res += tail_space * ' '
-        return res
+        spaces, tail = divmod(space_count, len(words) - 1)
+        return (' ' * spaces).join(words) + ' ' * tail
 
 
 sln = Solution()
