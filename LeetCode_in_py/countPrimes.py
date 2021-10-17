b4cefa5245999e65c2c5a2838c939579f59328fc
Yu4n <28648731+Yu4n@users.noreply.github.com>
2021-10-17 17:31:24 +0800

    countPrimes.py sieve method

---
+++ LeetCode_in_py/countPrimes.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n <= 2:
+            return 0
+        is_primes = [True] * (n + 1)
+        non_primes = 0
+        for i in range(2, int(n ** 0.5 + 1)):
+            if is_primes[i]:
+                for j in range(i * i, n, i):
+                    if is_primes[j]:
+                        is_primes[j] = False
+                        print("j: " + str(j))
+                        non_primes += 1
+        print(is_primes[2:-1])
+        print(len(is_primes[2:-1]))
+        print("non_primes is: " + str(non_primes))
+        return n - 2 - non_primes
+
+
+sln = Solution()
+print(sln.countPrimes(13))
