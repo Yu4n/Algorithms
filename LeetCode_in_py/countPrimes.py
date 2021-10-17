5f683d9aa06ee5e04785096475ec5f3274f58bba
Yu4n <28648731+Yu4n@users.noreply.github.com>
2021-10-17 19:00:36 +0800

    countPrimes.py list slice

--- LeetCode_in_py/countPrimes.py
+++ LeetCode_in_py/countPrimes.py
@@ -1,17 +1,13 @@
-from math import sqrt
-
-
 class Solution:
-    def countPrimes(self, n):
-        if n < 3:
+    def countPrimes(self, n: int) -> int:
+        if n <= 2:
             return 0
-        primes = [True] * n
-        primes[0] = primes[1] = False
-        for i in range(2, int(n ** 0.5) + 1):
-            if primes[i]:
-                primes[i * i: n: i] = [False] * len(primes[i * i: n: i])
-        return sum(primes)
+        is_primes = [True] * n
+        for i in range(2, int(n ** 0.5 + 1)):
+            if is_primes[i]:
+                is_primes[i * i:n:i] = [False] * len(is_primes[i * i:n:i])
+        return sum(is_primes[2:])
 
 
 sln = Solution()
-print(sln.countPrimes(4))
+print(sln.countPrimes(13))
