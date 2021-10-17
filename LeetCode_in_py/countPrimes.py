49896ae8386cc19d277e20c25271a79015de9324
Yu4n <28648731+Yu4n@users.noreply.github.com>
2021-10-17 17:38:47 +0800

    countPrimes.py remove unnecessary list size

--- LeetCode_in_py/countPrimes.py
+++ LeetCode_in_py/countPrimes.py
@@ -2,7 +2,7 @@ class Solution:
     def countPrimes(self, n: int) -> int:
         if n <= 2:
             return 0
-        is_primes = [True] * (n + 1)
+        is_primes = [True] * n
         non_primes = 0
         for i in range(2, int(n ** 0.5 + 1)):
             if is_primes[i]:
@@ -11,7 +11,7 @@ class Solution:
                         is_primes[j] = False
                         print("j: " + str(j))
                         non_primes += 1
-        print(is_primes[2:-1])
+        print(is_primes)
         print(len(is_primes[2:-1]))
         print("non_primes is: " + str(non_primes))
         return n - 2 - non_primes
