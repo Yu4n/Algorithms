bd13fab077651eeff5cab2b354109420fe6d8b85
Yu4n <28648731+Yu4n@users.noreply.github.com>
2021-10-21 14:33:07 +0800

    Update nextGreaterElement.py

--- LeetCode_in_py/nextGreaterElement.py
+++ LeetCode_in_py/nextGreaterElement.py
@@ -1,25 +1,16 @@
-from collections import deque
-
-
 class Solution:
     def nextGreaterElement(self, nums1: [int], nums2: [int]) -> [int]:
-        # 先遍历大数组nums2，首先将第一个元素入栈；
-        # 继续遍历，当当前元素小于栈顶元素时，继续将它入栈；当当前元素大于栈顶元素时，栈顶元素出栈，此时应将该出栈的元素与当前元素形成key - value键值对，存入HashMap中；
-        # 当遍历完nums2后，得到nums2中元素所对应的下一个更大元素的hash表；
-        # 遍历nums1的元素在hashMap中去查找‘下一个更大元素’，当找不到时则为 - 1。
-        hmap = {}
-        stack = deque()
+        greater_map = {x: -1 for x in nums1}
+        stack = []
 
-        for i in range(len(nums2)):
-            while stack and stack[-1] < nums2[i]:
-                hmap[stack.pop()] = nums2[i]
-            stack.append(nums2[i])
+        for num in nums2:
+            while stack and stack[-1] < num:
+                prev_num = stack.pop()
+                if prev_num in greater_map:
+                    greater_map[prev_num] = num
+            stack.append(num)
 
-        res = [-1] * len(nums1)
-        for i in range(len(nums1)):
-            if nums1[i] in hmap:
-                res[i] = hmap[nums1[i]]
-        return res
+        return [greater_map[x] for x in nums1]
 
 
 nums1 = [4, 1, 2]
